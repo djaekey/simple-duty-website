252658739dcb4e0743ef64151c3c93a60ae621b3
Guek Ling <yeoyueling@gmail.com>
2017-10-28 15:03:58 +0800

    Done Problem Reports Views & Controller (index & new)

--- app/controllers/problem_reports_controller.rb
+++ app/controllers/problem_reports_controller.rb
@@ -1,2 +1,33 @@
 class ProblemReportsController < ApplicationController
+	def index
+		@problem_reports = ProblemReport.all
+	end
+
+	def new
+  		@problem_reports = ProblemReport.new
+  		@users = User.all
+
+	end
+
+	def create
+		problem_report = ProblemReport.new problem_report_params
+		problem_report.date = DateTime.now
+		problem_report.status = "Broken"
+
+
+	  if problem_report.save
+    	redirect_to problem_reports_path
+	  else
+	    redirect_to new_problem_reports_path
+	  end
+	end
+
+	def edit
+		@problem_reports = ProblemReport.find params[:id]
+	end
+
+	private
+		def problem_report_params
+		  params.require(:problem_report).permit(:user_id, :computer_number, :details)
+		end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+	has_many :problem_reports
+	has_many :duty_schedules
 end
